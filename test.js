4428ec264106a968804be01543cb61dab3bec42a
KEVAL <koreaminsoo@naver.com>
2024-09-14 22:42:45 +0900

    Create test.js

---
+++ test.js
@@ -0,0 +1,14 @@
+function getRandomNickname() {
+    const adjectives = [
+        '멋진', '이상한', '쾌활한', '귀여운', '신비한', '빠른', '용감한', '미래적인', '어두운', '빛나는'
+    ];
+    
+    const nouns = [
+        '달팽이', '스마트폰', '리모컨', '자동차', '게임기', '책', '열쇠', '인형', '로봇', '모자'
+    ];
+    
+    const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+    const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+    
+    return `${randomAdjective} ${randomNoun}`;
+}
